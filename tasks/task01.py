3b20e43f20faf88dbdff74af8a44575b308db38e
Hrustyaschiy <resp1995@mail.ru>
2022-03-17 19:08:08 +1000

    Redo hw1 for better effectiveness

---
+++ tasks/task01.py
@@ -0,0 +1,37 @@
+from functools import lru_cache
+from functools import wraps
+from typing import Any
+from typing import List
+from typing import Callable
+
+
+def cache(times: int = 1) -> Callable:
+    def decorated_function(func: Callable) -> Callable:
+        times_called = times
+        result = None
+
+        def cached_func(*args: List[Any]):
+            nonlocal times_called
+            nonlocal result
+            if times_called == times:
+                result = func(*args)
+                times_called = 0
+            else:
+                times_called += 1
+            return result
+
+        return cached_func
+
+    return decorated_function
+
+
+@cache(times=2)
+def f() -> str:
+    return input('? ')
+
+
+print(f())
+print(f())
+print(f())
+print(f())
+print(f())
